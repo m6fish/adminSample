67fede8ce37e57340d883c743cc691ed3c5c449c
bulb <pumpkinjack@livemail.tw>
2020-10-14 22:54:43 +0800

    環境初始化

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  lintOnSave: true,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@V', resolve('src/views'))
+      .set('@C', resolve('src/components'))
+  },
+}
